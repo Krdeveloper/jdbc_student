c8c7a07efc4f85c8e58762c9dabd1c7a34c00791
KM <krdeveloper7@gmail.com>
2017-03-08 11:29:31 +0900

    DataBase Setting Complete... DbConnection Test Complete...

---
+++ src/test/java/kr/or/dgit/jdbc/DbConnectionTest.java
@@ -0,0 +1,31 @@
+package kr.or.dgit.jdbc;
+
+import static org.junit.Assert.fail;
+
+import java.sql.Connection;
+
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import kr.or.dgit.jdbc.util.ConnectionFactory;
+
+public class DbConnectionTest {
+	private static Connection connection;
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		connection = ConnectionFactory.getInstance();
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+		connection = null;
+	}
+
+	@Test
+	public void testConnection() {
+		Assert.assertNotNull(connection);
+	}
+
+}
